6ba9a2a644d1fbd9331f96c78524a08835a26e5d
kellymraz <kitty8mars@gmail.com>
2015-08-07 10:37:47 -0600

    final draft

--- public/js/main.js
+++ public/js/main.js
@@ -7,20 +7,10 @@ var tour = new Shepherd.Tour({
   }
 });
 
-tour.addStep('example-step', {
-  title: 'Follow along as we explain the features of this website',
-  text: 'Click here to submit your own photo.',
-  attachTo: '#addButton',
-  // classes: 'example-step-extra-class',
-  buttons: [
-    {
-      text: 'Next',
-      action: tour.next
-    }
-  ]
-});
+
 
 tour.addStep('example-step', {
+  title: 'Follow along as we explain the features of this website',
   text: 'Here is a photo of a shopper.  What caption would you add?',
   attachTo: '#first-image left',
   // classes: 'example-step-extra-class',
@@ -34,7 +24,7 @@ tour.addStep('example-step', {
 
 tour.addStep('example-step', {
   text: 'Click on the button below the image to add YOUR caption',
-  attachTo: '#captionButton1',
+  attachTo: '#captionButton1 bottom',
   // classes: 'example-step-extra-cass',
   buttons: [
     {
@@ -45,7 +35,7 @@ tour.addStep('example-step', {
 });
 
 tour.addStep('example-step', {
-  text: 'Big Jim - He is ready for a night on the town ... with you.',
+  text: 'Can you think of a caption for this one?',
   attachTo: '#third-image right',
   // classes: 'example-step-extra-class',
   buttons: [
@@ -57,8 +47,8 @@ tour.addStep('example-step', {
 });
 
 tour.addStep('example-step', {
-  text: 'Mrs. Talons - Do NOT play footsy with her.',
-  attachTo: '#fourth-image top',
+  text: 'Do you have a photo to add that is as thought-provoking as this one?.',
+  attachTo: '#fourth-image left',
   // classes: 'example-step-extra-class',
   buttons: [
     {
@@ -68,15 +58,29 @@ tour.addStep('example-step', {
   ]
 });
 
+// tour.addStep('example-step', {
+//   text: 'Elvis is NOT dead - But he does buy his string cheese at Walmart',
+//   attachTo: '#fifth-image left',
+//   // classes: 'example-step-extra-class',
+//   buttons: [
+//     {
+//       text: 'Next',
+//       action: tour.next
+//     }
+//   ]
+// });
+
 tour.addStep('example-step', {
-  text: 'Elvis is NOT dead - But he does buy his string cheese at Walmart',
-  attachTo: '#fifth-image left',
+  
+  text: 'Click here to submit your own photo.',
+  attachTo: '#addButton',
   // classes: 'example-step-extra-class',
   buttons: [
     {
-      text: 'Next',
+      text: 'End of Tour-- Have fun!!!',
       action: tour.next
     }
   ]
 });
+
 tour.start();
